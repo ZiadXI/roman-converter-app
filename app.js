4359c5457fbbe9e93beec4bf819e4c7d12a8f32c
Ziad Kassem <ziadxkassem123@gmail.com>
2025-07-13 01:17:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-const express = require('express');
-const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Welcome to homee");
-});
-
-app.listen(4000, () => {
-    console.log("Server is running on port 4000");
-});
-use('mydb');
-db.users.insertOne({ name: "Ziad" });
